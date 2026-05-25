c9af0949b41757da0ace582042b8cc602a1805cb
jfontdev <jfontdev@users.noreply.github.com>
2026-05-25 15:17:04 +0200

    feat: remove Tag, Playlist and Playlist-related dead code

--- src/main/java/com/jfontdev/trackstack/dto/tag/TagRequestDTO.java
+++
@@ -1,7 +0,0 @@
-package com.jfontdev.trackstack.dto.tag;
-
-import jakarta.validation.constraints.NotBlank;
-
-public record TagRequestDTO(
-                @NotBlank(message = "Name must not be empty") String name) {
-}
